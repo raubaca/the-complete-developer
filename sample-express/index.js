4bf90c470ae71554c6167fa6655452a9e5798b67
raubaca <raubaca@hotmail.com>
2025-10-20 12:04:18 -0400

    Ch3 - TypeScript

--- sample-express/index.js
+++ sample-express/index.js
@@ -1,24 +1,34 @@
-import { routeHello, routeAPINames } from './routes.js';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { routeHello, routeAPINames, routeWeather } from './routes.js';
 import express from 'express';
-
 const server = express();
 const port = 3000;
-
-server.get('/hello', (req, res) => {
-  const response = routeHello(req, res);
-  res.send(response);
+server.get('/hello', (_req, res) => {
+    const response = routeHello();
+    res.send(response);
 });
-
-server.get('/api/names', async (req, res) => {
-  let response;
-  try {
-    response = await routeAPINames(req, res);
-  } catch (err) {
-    console.log(err);
-  }
-  res.send(response);
+server.get('/api/names', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let response;
+    try {
+        response = yield routeAPINames();
+    }
+    catch (err) {
+        console.log(err);
+    }
+    res.send(response);
+}));
+server.get('/api/weather/:zipcode', (req, res) => {
+    const response = routeWeather({ zipcode: req.params.zipcode });
+    res.send(response);
 });
-
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
---
+++ sample-express/index.ts
@@ -0,0 +1,32 @@
+import { routeHello, routeAPINames, routeWeather } from './routes.js';
+import express, { Request, Response } from 'express';
+
+const server = express();
+const port = 3000;
+
+server.get('/hello', (_req: Request, res: Response): void => {
+  const response = routeHello();
+  res.send(response);
+});
+
+server.get(
+  '/api/names',
+  async (_req: Request, res: Response): Promise<void> => {
+    let response;
+    try {
+      response = await routeAPINames();
+    } catch (err) {
+      console.log(err);
+    }
+    res.send(response);
+  }
+);
+
+server.get('/api/weather/:zipcode', (req: Request, res: Response): void => {
+  const response = routeWeather({ zipcode: req.params.zipcode });
+  res.send(response);
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
